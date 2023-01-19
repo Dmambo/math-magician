ebd28db5f272d45da9a714d84e3cfd1c1e40f24a
Dmambo <mambotrader0@gmail.com>
2023-01-19 14:10:40 +0530

    Refactor my code with Hooks

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,64 +1,175 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './calculator.css';
 import calculate from './logic/calculate';
 
-export default class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  clickHandler = (e) => {
-    const result = calculate(this.state, e.target.textContent);
-    this.setState((prevState) => ({
+  const clickHandler = (e) => {
+    const result = calculate(state, e.target.textContent);
+    setState((prevState) => ({
       ...prevState,
       ...result,
     }));
   };
 
-  render() {
-    const { total, next } = this.state;
-    return (
-      <div className="calculator">
-        <div className="calculator__display">
-          <div className="calculator__display__curr">{ next || total || 0}</div>
+  const { total, next } = state;
+  return (
+    <div className="calculator">
+      <div className="calculator__display">
+        <div className="calculator__display__curr">{next || total || 0}</div>
+      </div>
+      <div className="calculator__buttons">
+        <div className="calculator__buttons__row">
+          <button
+            type="button"
+            className="calculator__buttons__row__button"
+            onClick={clickHandler}
+          >
+            AC
+          </button>
+          <button
+            type="button"
+            className="calculator__buttons__row__button"
+            onClick={clickHandler}
+          >
+            +/-
+          </button>
+          <button
+            type="button"
+            className="calculator__buttons__row__button"
+            onClick={clickHandler}
+          >
+            %
+          </button>
+          <button
+            type="button"
+            className="calculator__buttons__row__button orange"
+            onClick={clickHandler}
+          >
+            &#247;
+          </button>
         </div>
-        <div className="calculator__buttons">
-          <div className="calculator__buttons__row">
-            <button type="button" className="calculator__buttons__row__button" onClick={this.clickHandler}>AC</button>
-            <button type="button" className="calculator__buttons__row__button" onClick={this.clickHandler}>+/-</button>
-            <button type="button" className="calculator__buttons__row__button" onClick={this.clickHandler}>%</button>
-            <button type="button" className="calculator__buttons__row__button orange" onClick={this.clickHandler}>&#247;</button>
-          </div>
-          <div className="calculator__buttons__row">
-            <button type="button" className="calculator__buttons__row__button" onClick={this.clickHandler}>7</button>
-            <button type="button" className="calculator__buttons__row__button" onClick={this.clickHandler}>8</button>
-            <button type="button" className="calculator__buttons__row__button" onClick={this.clickHandler}>9</button>
-            <button type="button" className="calculator__buttons__row__button orange" onClick={this.clickHandler}>*</button>
-          </div>
-          <div className="calculator__buttons__row">
-            <button type="button" className="calculator__buttons__row__button" onClick={this.clickHandler}>4</button>
-            <button type="button" className="calculator__buttons__row__button" onClick={this.clickHandler}>5</button>
-            <button type="button" className="calculator__buttons__row__button" onClick={this.clickHandler}>6</button>
-            <button type="button" className="calculator__buttons__row__button orange" onClick={this.clickHandler}>-</button>
-          </div>
-          <div className="calculator__buttons__row_">
-            <button type="button" className="calculator__buttons__row__button" onClick={this.clickHandler}>1</button>
-            <button type="button" className="calculator__buttons__row__button" onClick={this.clickHandler}>2</button>
-            <button type="button" className="calculator__buttons__row__button" onClick={this.clickHandler}>3</button>
-            <button type="button" className="calculator__buttons__row__button orange" onClick={this.clickHandler}>+</button>
-          </div>
-          <div className="calculator__buttons__row">
-            <button type="button" className="calculator__buttons__row__button zero" onClick={this.clickHandler}>0</button>
-            <button type="button" className="calculator__buttons__row__button" onClick={this.clickHandler}>.</button>
-            <button type="button" className="calculator__buttons__row__button orange" onClick={this.clickHandler}>=</button>
-          </div>
+        <div className="calculator__buttons__row">
+          <button
+            type="button"
+            className="calculator__buttons__row__button"
+            onClick={clickHandler}
+          >
+            7
+          </button>
+          <button
+            type="button"
+            className="calculator__buttons__row__button"
+            onClick={clickHandler}
+          >
+            8
+          </button>
+          <button
+            type="button"
+            className="calculator__buttons__row__button"
+            onClick={clickHandler}
+          >
+            9
+          </button>
+          <button
+            type="button"
+            className="calculator__buttons__row__button orange"
+            onClick={clickHandler}
+          >
+            *
+          </button>
+        </div>
+        <div className="calculator__buttons__row">
+          <button
+            type="button"
+            className="calculator__buttons__row__button"
+            onClick={clickHandler}
+          >
+            4
+          </button>
+          <button
+            type="button"
+            className="calculator__buttons__row__button"
+            onClick={clickHandler}
+          >
+            5
+          </button>
+          <button
+            type="button"
+            className="calculator__buttons__row__button"
+            onClick={clickHandler}
+          >
+            6
+          </button>
+          <button
+            type="button"
+            className="calculator__buttons__row__button orange"
+            onClick={clickHandler}
+          >
+            -
+          </button>
+        </div>
+        <div className="calculator__buttons__row_">
+          <button
+            type="button"
+            className="calculator__buttons__row__button"
+            onClick={clickHandler}
+          >
+            1
+          </button>
+          <button
+            type="button"
+            className="calculator__buttons__row__button"
+            onClick={clickHandler}
+          >
+            2
+          </button>
+          <button
+            type="button"
+            className="calculator__buttons__row__button"
+            onClick={clickHandler}
+          >
+            3
+          </button>
+          <button
+            type="button"
+            className="calculator__buttons__row__button orange"
+            onClick={clickHandler}
+          >
+            +
+          </button>
+        </div>
+        <div className="calculator__buttons__row">
+          <button
+            type="button"
+            className="calculator__buttons__row__button zero"
+            onClick={clickHandler}
+          >
+            0
+          </button>
+          <button
+            type="button"
+            className="calculator__buttons__row__button"
+            onClick={clickHandler}
+          >
+            .
+          </button>
+          <button
+            type="button"
+            className="calculator__buttons__row__button orange"
+            onClick={clickHandler}
+          >
+            =
+          </button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Calculator;
